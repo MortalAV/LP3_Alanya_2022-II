257ff94b03d0e56b98da5281dde52e735d100793
Joel Alanya <joel23alanya@gmail.com>
2022-06-01 11:41:06 -0500

    UC2 - Gestíón de Archivos de Joel Alanya

---
+++ UC2/gestion_de_archivos.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  1 11:33:07 2022
+
+@author: Equipo
+"""
+
+import os 
+
+# Para crear archivo, recibe como parámetro el nombre del archivo
+# y el contenido del archivo
+def crear_archivo(nombre, contenido):
+    archivo=open(nombre,"wt")
+    archivo.write(contenido)
+    archivo.close()
+    
+# Para eliminar recibe como parámetro el nombre del archivo a eliminar
+def eliminar_archivo(nombre):
+    os.remove(nombre)
+    
+# Para agregar contenido a un archivo plano, debe existir un archivo
+# Se envia como parámetro el nombre y el contenido del archivo
+def agregar_contenido_archivo(nombre, contenido):
+    archivo = open(nombre,"at")
+    archivo.write("\n"+contenido)
+    
+# Para leer un archivo, ya debe existir un archivo plano 
+# Ejemplo: txt, py, java, ...
+# Recibe como parámetro el nombre del archivo a leer
+# y devuelve el contenido del archivo
+def leer_archivo(nombre):
+    archivo = open(nombre,"rt",encoding='utf8')
+    contenido = archivo.read()
+    return contenido
